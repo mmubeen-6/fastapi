0d806b4ef752430591f648793edcd00a54c4c578
Muhammad Mubeen <mubeen@Mubeens-MBP.local>
2023-08-26 14:42:22 +0500

    test script fixes

--- scripts/run_config_docker.sh
+++ scripts/run_config_docker.sh
@@ -2,5 +2,5 @@
 
 docker run -it --network fastapi_default \
     --env-file .env -p 8000:8000 \
-    -v /Users/mubeen/Workspace/fastapi/services/config/app:/app \
-    postsservice:latest bash;
+    -v /Users/mubeen/Workspace/fastapi/services/postsservice/app:/app \
+    postsservice:latest uvicorn main:app --host 0.0.0.0 --reload;
